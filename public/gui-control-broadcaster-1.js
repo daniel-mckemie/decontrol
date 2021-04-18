c5be1417ebbd71f498213ae8eb75ee3574d36fa6
Daniel-McKemie <daniel.mckemie@gmail.com>
2021-04-18 16:36:30 -0400

    test1 - fade disarmed

--- public/gui-control-broadcaster-1.js
+++ public/gui-control-broadcaster-1.js
@@ -51,18 +51,19 @@ WebMidi.enable(function (err) {
       osc2Freq: document.getElementById('slider2'),      
       filterFreq: document.getElementById('slider3'),
       filterRes: document.getElementById('slider4'),
-      amp1Ch1: document.getElementById('slider5'),
-      amp2Ch1: document.getElementById('slider6'),
-      sAndHGate: document.getElementById('slider7'),
+      amp1Ch1: document.getElementById('slider5'),      
+      sAndHGate: document.getElementById('slider6'),
+      envGate: document.getElementById('slider7'),
+      switchTrig: document.getElementById('slider8'),
       // TBD: document.getElementById('TBD'),
       
     }
     
-    
-    for (const property in allParams) {
-      allParams[property].style.visibility = 'hidden';
-      // console.log(allParams[property].style.visibility);      
-    }
+    // UNCOMMENT TO HIDE
+    // for (const property in allParams) {
+    //   allParams[property].style.visibility = 'hidden';
+    //   // console.log(allParams[property].style.visibility);      
+    // }
 
 
     allParams.osc1Freq.addEventListener('input', function () {
@@ -80,82 +81,101 @@ WebMidi.enable(function (err) {
     allParams.amp1Ch1.addEventListener('input', function () {
       changeItDefault(38, 06, allParams.amp1Ch1.value, 5)
     });
-    allParams.amp2Ch1.addEventListener('input', function () {
-      changeItDefault(38, 06, allParams.amp2Ch1.value, 6)
-    });
-    let buttVal = 0;
+
+    let buttVal1 = 0;
     allParams.sAndHGate.addEventListener('click', function () {            
-      if (buttVal == 0) {
-        changeItDefault(38, 06, 1, 7);
+      if (buttVal1 == 0) {
+        changeItDefault(38, 06, 1, 6);
         allParams.sAndHGate.value = 'Trigger Sample & Hold - ON'
-        buttVal = 1;
+        buttVal1 = 1;
       }
       else {
-        changeItDefault(38, 06, 0, 7);
+        changeItDefault(38, 06, 0, 6);
         allParams.sAndHGate.value = 'Trigger Sample & Hold - OFF'
-        buttVal = 0;
+        buttVal1 = 0;
       }      
-    });    
+    });
+
+    let buttVal2 = 0;
+    allParams.envGate.addEventListener('click', function () {
+      if (buttVal2 == 0) {
+        changeItDefault(38, 06, 1, 7);
+        allParams.envGate.value = 'Envelope Gate - ON'
+        buttVal2 = 1;
+      } else {
+        changeItDefault(38, 06, 0, 7);
+        allParams.envGate.value = 'Envelope Gate - OFF'
+        buttVal2 = 0;
+      }
+    });
+    
+    let buttVal3 = 0;
+    allParams.switchTrig.addEventListener('click', function () {
+      if (buttVal3 == 0) {
+        changeItDefault(38, 06, 1, 8);
+        allParams.switchTrig.value = 'Switch - A'
+        buttVal3 = 1;
+      } else {
+        changeItDefault(38, 06, 0, 8);
+        allParams.switchTrig.value = 'Switch - B'
+        buttVal3 = 0;
+      }
+    });
+
     // allParams.TBD.addEventListener('change', function () {
     //   changeItDefault(38, 12, allParams.TBD.value, 8)
     // });
-    function fadeIn(element) {
-      var op = 0.1; // initial opacity      
-      var timer = setInterval(function () {
-        if (op >= 1) {
-          clearInterval(timer);
-          element.style.visibility = 'visible';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.05;
-      }, 10);
-    }
+    // function fadeIn(element) {
+    //   var op = 0.1; // initial opacity      
+    //   var timer = setInterval(function () {
+    //     if (op >= 1) {
+    //       clearInterval(timer);
+    //       element.style.visibility = 'visible';
+    //     }
+    //     element.style.opacity = op;
+    //     element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    //     op += op * 0.05;
+    //   }, 10);
+    // }
 
-    function fadeOut(element) {      
-      var op = 1; // initial opacity
-      var timer = setInterval(function () {
-        if (op <= 0.1) {
-          clearInterval(timer);
-          element.style.visibility = 'hidden';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.05;
-      }, 10);
-    }
+    // function fadeOut(element) {      
+    //   var op = 1; // initial opacity
+    //   var timer = setInterval(function () {
+    //     if (op <= 0.1) {
+    //       clearInterval(timer);
+    //       element.style.visibility = 'hidden';
+    //     }
+    //     element.style.opacity = op;
+    //     element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    //     op -= op * 0.05;
+    //   }, 10);
+    // }
 
-    randomIntervalFadeIn = 5000;
-    setInterval(() => {
-      randomIntervalFadeIn = Math.floor(Math.random() * 30000) + 3000;
-    }, 5000);
+    // randomIntervalFadeIn = 5000;
+    // setInterval(() => {
+    //   randomIntervalFadeIn = Math.floor(Math.random() * 30000) + 3000;
+    // }, 5000);
 
-    randomIntervalFadeOut = 1000;
-    setInterval(() => {
-      randomIntervalFadeOut = Math.floor(Math.random() * 1000);
-    }, 100);
+    // randomIntervalFadeOut = 1000;
+    // setInterval(() => {
+    //   randomIntervalFadeOut = Math.floor(Math.random() * 1000);
+    // }, 100);
 
     
-    setInterval(() => {      
-      let randomElement = Math.floor(Math.random() * Object.keys(allParams).length);      
-      if (allParams[Object.keys(allParams)[randomElement]].style.visibility == 'hidden') {
-        fadeIn(allParams[Object.keys(allParams)[randomElement]]);
-      }      
-    }, randomIntervalFadeIn);
+    // setInterval(() => {      
+    //   let randomElement = Math.floor(Math.random() * Object.keys(allParams).length);      
+    //   if (allParams[Object.keys(allParams)[randomElement]].style.visibility == 'hidden') {
+    //     fadeIn(allParams[Object.keys(allParams)[randomElement]]);
+    //   }      
+    // }, randomIntervalFadeIn);
     
-    setInterval(() => {         
-      let randomElement = Math.floor(Math.random() * Object.keys(allParams).length);
-      if (allParams[Object.keys(allParams)[randomElement]].style.visibility == 'visible') {
-        fadeOut(allParams[Object.keys(allParams)[randomElement]]);
-      }
-    }, randomIntervalFadeOut);    
+    // setInterval(() => {         
+    //   let randomElement = Math.floor(Math.random() * Object.keys(allParams).length);
+    //   if (allParams[Object.keys(allParams)[randomElement]].style.visibility == 'visible') {
+    //     fadeOut(allParams[Object.keys(allParams)[randomElement]]);
+    //   }
+    // }, randomIntervalFadeOut);    
   }
 
-
-  
-  // socket.on('videoTransport', function(data) {
-  //   // otherVideoTag.srcObject = data.stream;    
-  //   otherVideoTag.src = URL.createObjectURL(data.stream);
-  // })
 });
 
